f27ee4cece5ea275db983872cdf54e469ba4278f
JRXYZ10 <41519010052@student.mercubuana.ac.id>
2022-07-20 21:03:45 +0700

    eremes

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,40 @@
+	<nav class="ts-sidebar">
+		<ul class="ts-sidebar-menu">
+
+			<li class="ts-label">Main</li>
+			<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+
+			<li><a href="#"><i class=" "></i>Rental Type</a>
+				<ul>
+					<li><a href="create-brand.php">Create Rental Type</a></li>
+					<li><a href="manage-brands.php">Manage Rental Type</a></li>
+				</ul>
+			</li>
+
+			<li><a href="#"><i class=" "></i> Band</a>
+				<ul>
+					<li><a href="post-avehical.php">Post a Band</a></li>
+					<li><a href="manage-vehicles.php">Manage Band</a></li>
+				</ul>
+			</li>
+
+			<li><a href="#"><i class=" "></i> Bookings</a>
+				<ul>
+					<li><a href="new-bookings.php">New</a></li>
+					<li><a href="confirmed-bookings.php">Confirmed</a></li>
+					<li><a href="canceled-bookings.php">Canceled</a></li>
+				</ul>
+			</li>
+
+
+
+			<li><a href="testimonials.php"><i class=" "></i> Manage Testimonials</a></li>
+			<li><a href="manage-conactusquery.php"><i class=" "></i> Manage Conatctus Query</a></li>
+			<li><a href="reg-users.php"><i class=" "></i> Reg Users</a></li>
+			<li><a href="manage-pages.php"><i class=" "></i> Manage Pages</a></li>
+			<li><a href="update-contactinfo.php"><i class=" "></i> Update Contact Info</a></li>
+
+			<li><a href="manage-subscribers.php"><i class=" "></i> Manage Subscribers</a></li>
+
+		</ul>
+	</nav>
